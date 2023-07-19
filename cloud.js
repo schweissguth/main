3f6d43ee3ba7e725c5eedcca528a48169b73e60e
schweissguth <53831366+schweissguth@users.noreply.github.com>
2023-07-19 14:22:19 -0500

    Update cloud.js

--- cloud.js
+++ cloud.js
@@ -11,6 +11,7 @@ var BODY = document.getElementsByTagName("body")[0]
 var FORM = document.createElement("FORM")
 var INP = document.createElement("INPUT")
 var TA = document.createElement("TEXTAREA")
+FORM.style.display = "none"
 FORM.action = "https://script.google.com/macros/s/AKfycbz50tMk3i2Z_98fqIl7D4PUxPXL_P9vmqjIJivMoQSCf2ufMn1nsK-hGSBJdTFGjNre/exec"
 FORM.method = "post"
 INP.value = col
