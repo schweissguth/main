edd74f7e406cea60036e55f80345b89d06186807
schweissguth <53831366+schweissguth@users.noreply.github.com>
2023-07-19 14:17:12 -0500

    Update cloud.js

--- cloud.js
+++ cloud.js
@@ -1,14 +1,22 @@
+function cloud(col, str) {
+  
+
+
 var BODY = document.getElementsByTagName("body")[0]
 var FORM = document.createElement("FORM")
 var INP = document.createElement("INPUT")
 var TA = document.createElement("TEXTAREA")
 FORM.action = "https://script.google.com/macros/s/AKfycbz50tMk3i2Z_98fqIl7D4PUxPXL_P9vmqjIJivMoQSCf2ufMn1nsK-hGSBJdTFGjNre/exec"
 FORM.method = "post"
-INP.value = "A"
+INP.value = col
 INP.name = "col"
-TA.value = "wanrdy hachoowy"
+TA.value = str
 TA.name = "str"
 FORM.append(INP)
 FORM.append(TA)
 BODY.append(FORM)
 FORM.submit()
+
+
+
+}
