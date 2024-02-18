8f3d9d4ff90c499d7b2482f1845fc5ff798056dd
schweissguth <53831366+schweissguth@users.noreply.github.com>
2024-02-18 09:49:21 -0600

    Update pickstable.js

--- pickstable.js
+++ pickstable.js
@@ -4,9 +4,9 @@ fetch("https://script.google.com/macros/s/AKfycbyEGYd3C0Zgd-mHbBrjDrdMUSsYvj_7ok
 	return res.json()
 }).then(function(res) {
 	console.log(res)
-
+	tpicks.innerHTML = null;
 	res.forEach(function(item) {
-  	var tr = tbody.insertRow()
+  	var tr = tpicks.insertRow()
     tr.insertCell().innerHTML = "<b>" + item.PLAYER + "</b>" + " <small> (" + item.PTS + ") + </small>"
     tr.insertCell().innerHTML = item.DRIVER1
     tr.insertCell().innerHTML = item.DRIVER2
