cf0ec2753189913a1e551b80ca2935a33df0bb2e
schweissguth <53831366+schweissguth@users.noreply.github.com>
2024-02-18 09:43:28 -0600

    Create live.js

---
+++ live.js
@@ -0,0 +1,16 @@
+function getLiveRace() {
+  fetch("https://script.google.com/macros/s/AKfycbyEGYd3C0Zgd-mHbBrjDrdMUSsYvj_7oky1yIuffDrRk8rURlR-gV_IteIJHzznntfO/exec?page=liverace").then(function(res) {
+    return res.json()
+  }).then(function(res) {
+    console.log(res)
+    tlive.innerHTML = ""
+    res.vehicles.forEach(function(item) {
+    	var tr = tlive.insertRow()
+      tr.insertCell().innerHTML = item.driver.full_name
+      tr.insertCell().innerHTML = item.PTS
+      tr.insertCell().innerHTML = item.PLAYER
+    })
+  })
+}
+
+getLiveRace()
