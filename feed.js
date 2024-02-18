08177d2a56e44d0b649f2278fb64f063d4c15f3f
schweissguth <53831366+schweissguth@users.noreply.github.com>
2024-02-18 14:36:08 -0600

    Create feed.js

---
+++ feed.js
@@ -0,0 +1,16 @@
+      fetch("https://cf.nascar.com/cacher/2023/1/" + 5275 + "/lap-notes.json").then(function (item) {
+        return item.json()
+      }).then(function (item) {
+        console.log(item.laps)
+
+        for (var i in item.laps) {
+          item.laps[i].forEach(function (jtem) {
+            var tr = tfeed.insertRow(0)
+            tr.insertCell().innerHTML = i
+            tr.insertCell().innerHTML = jtem.Note
+            
+          })
+
+        }
+      })
+    
