2b301f3d504f88f44ef89d4de4260b85b1aa50a7
schweissguth <53831366+schweissguth@users.noreply.github.com>
2024-03-09 23:15:38 -0600

    Update feed.js

--- feed.js
+++ feed.js
@@ -3,7 +3,7 @@ var raceno = 0
 function getFeed() {
 
 fetch("https://cf.nascar.com/cacher/live/live-feed.json").then(res => res.json()).then(function(res) {
-	console.log(res)
+	//console.log(res)
   raceno = res.race_id
   //raceno = 5275
       getRace()
@@ -15,7 +15,7 @@ function getRace() {
 fetch("https://cf.nascar.com/cacher/2024/1/" + raceno + "/lap-notes.json").then(function (item) {
         return item.json()
       }).then(function (item) {
-        console.log(item.laps)
+        //console.log(item.laps)
             tfeed.innerHTML = null
 
         for (var i in item.laps) {
