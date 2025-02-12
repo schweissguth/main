4974a3377fdc721f15c9e9a4564daf503b01f61c
schweissguth <53831366+schweissguth@users.noreply.github.com>
2025-02-12 16:06:49 -0600

    Update picks.js

--- picks.js
+++ picks.js
@@ -28,42 +28,8 @@ function makeTable() {
     
     var td = tr.insertCell()
     
-    var form = document.createElement("form")
-    td.append(form)
-    form.action = "https://script.google.com/macros/s/AKfycbwyx5JZpneWX4L_GyEike2Z8Bqjmgy-8qrNOTVORBSi1ErN6AWO7E8rNM8HIc5bS2nW/exec?PICKS"
-    form.method = "post"
-    form.target = "iframe"
-    form.id = "form"
-    
-    var iframe = document.createElement("iframe")
-    iframe.name = "iframe"
-    iframe.style.display = "none"
-    form.append(iframe)
-    
-    var inpage = document.createElement("input")
-    inpage.name = "page"
-    inpage.value = "picksubmit"
-    inpage.type = "hidden"
-    form.append(inpage)
-    
-    var inp = document.createElement("input")
-    inp.name = "driverid"
-    inp.value = item.driver_id
-    inp.type = "hidden"
-    form.append(inp)
-    
-    var inprid = document.createElement("input")
-    inprid.name = "raceid"
-    inprid.value = item.RACEID
-    inprid.type = "hidden"
-    form.append(inprid)
-    
-    var select = document.createElement("select")
-    select.name = "playerid"
-    select.onchange = function() {
-    	form.submit()
-    }
-    var option = document.createElement("option")
+    var select = document.createElement("SELECT")
+    var option = document.createElement("OPTION")
     select.append(option)
     
     PLAYERS.forEach(function(player) {
@@ -72,8 +38,23 @@ function makeTable() {
       opt.value = player.ID
       select.append(opt)
     })
-    form.append(select)
+    
+    select.onchange = function() {
+    	fetch("https://script.google.com/macros/s/AKfycbwyx5JZpneWX4L_GyEike2Z8Bqjmgy-8qrNOTVORBSi1ErN6AWO7E8rNM8HIc5bS2nW/exec?PICKS", {
+      	method: "post",
+        body: JSON.stringify({
+        	ID: Date.now(),
+          RACEID: item.RACEID,
+          DRIVERID: item.driver_id,
+          PLAYERID: this.value,
+        })
+      })
+    }
     select.value = item.PLAYERID
+    td.append(select)
+    
+    
+ 
     
   })
 var tr = tbody.insertRow(13)
