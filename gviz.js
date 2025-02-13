eb5c67fdf8d1e6356111fd21670a299f2a5f3e75
schweissguth <53831366+schweissguth@users.noreply.github.com>
2025-02-13 11:02:15 -0600

    Update gviz.js

--- gviz.js
+++ gviz.js
@@ -1,22 +1,19 @@
-function gviz(obj) {
-	obj = obj.split("setResponse(")[1].slice(0, -2)
-  obj = JSON.parse(obj)
-  obj = obj.table
-  console.log(obj)
-  var rows = obj.rows
-  console.log("rows", rows)
-  var cols = obj.cols
-  console.log("cols", cols)
-
-  var arr = []
-  
-  rows.forEach(function(row) {
-    var newobj = {}
-    console.log("row", row)
-  	row.c.forEach(function(cell, c) {
-      newobj[cols[c].label] = cell.v
+function gviz(res) {
+  res = res.split("setResponse(")[1]
+  res = res.replace(");", "")
+  res = JSON.parse(res)
+  res = res.table.rows
+  res = res.map(function(row) {
+  	return row.c.map(function(cell) {
+    	return cell.v
     })
-    arr.push(newobj)
   })
-  return arr
+  var header = res.shift()
+  res = res.map(function(row, r) {
+  	return row.reduce(function(curr, acc, i) {
+    	curr[header[i]] = acc
+    	return curr
+    }, {})
+  })
+  return res
 }
