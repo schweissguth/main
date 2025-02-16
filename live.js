54f421d59b443bacb8ba5759c611a5780e3d9cd5
schweissguth <53831366+schweissguth@users.noreply.github.com>
2025-02-16 13:25:08 -0600

    Update live.js

--- live.js
+++ live.js
@@ -36,7 +36,8 @@ function makeTable(feeds) {
 	feeds.vehicles.forEach(function(feed) {
   	var tr = tlive.insertRow()
     tr.insertCell().innerText = feed.running_position + ")"
-    tr.insertCell().innerText = feed.driver.full_name
+    tr.insertCell().innerHTML = feed.driver.full_name + " " + "<i><small>-" + Math.round(feed.delta * 100)/100 + "</small></i>"
     tr.insertCell().innerHTML = "<b>" + feed.PLAYERNAME + "</b>"
   })
+  
 }
