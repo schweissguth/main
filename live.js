241185aa905878a66fcb3b0a550c7c2ad02c9982
schweissguth <53831366+schweissguth@users.noreply.github.com>
2025-02-16 13:15:13 -0600

    Update live.js

--- live.js
+++ live.js
@@ -1,32 +1,42 @@
-function getLiveRace() {
-  fetch("https://script.google.com/macros/s/AKfycbyEGYd3C0Zgd-mHbBrjDrdMUSsYvj_7oky1yIuffDrRk8rURlR-gV_IteIJHzznntfO/exec?LIVERACE").then(function(res) {
-    return res.json()
-  }).then(function(res) {
-    //console.log(res)
-    tlive.innerHTML = ""
-    res.vehicles.forEach(function(item, index) {
-      item.delta = parseFloat(item.delta).toFixed(2)
-      if (index == 0) {
-        item.delta = ""
 
-      }
-      var tr = tlive.insertRow()
-      tr.insertCell().innerHTML = index + 1
-      tr.insertCell().innerHTML = item.driver.full_name + "<small> " + item.delta +"</small>"
-      if (item.PTS > 0) {
-      tr.insertCell().innerHTML = item.PTS
-      } else {
-      tr.insertCell().innerHTML = ""
-        
-      }
-      tr.insertCell().innerHTML = "<b>" + item.PLAYER + "</b>"
-    })
+
+function getLiveRace() {
+	fetch("https://cf.nascar.com/cacher/live/live-feed.json").then(function(res) {
+  	return res.json()
+  }).then(function(feeds) {
+  	tlive.innerHTML = null
+  	console.log(feeds)
+    getDrivers(feeds)
   })
 }
 
 getLiveRace()
 
 
-setInterval(function(){
-  getLiveRace()
-}, 30000)
+function getDrivers(feeds) {
+	fetch("https://script.google.com/macros/s/AKfycbyEGYd3C0Zgd-mHbBrjDrdMUSsYvj_7oky1yIuffDrRk8rURlR-gV_IteIJHzznntfO/exec?DRIVERS").then(function(drivers) {
+  	return drivers.json()
+  }).then(function(drivers) {
+  	console.log(drivers)
+    feeds.vehicles = feeds.vehicles.map(function(feed) {
+    	var find = drivers.find(function(driver) {
+      	return feed.driver.driver_id == driver.DRIVERID
+      }) || {
+      	PLAYERNAME: ""
+      }
+      feed.PLAYERNAME = find.PLAYERNAME
+      return feed
+    })
+    makeTable(feeds)
+  })
+}
+
+
+function makeTable(feeds) {
+	feeds.vehicles.forEach(function(feed) {
+  	var tr = tlive.insertRow()
+    tr.insertCell().innerText = feed.running_position
+    tr.insertCell().innerText = feed.driver.full_name
+    tr.insertCell().innerHTML = feed.PLAYERNAME
+  })
+}
