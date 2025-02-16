6af3ed44e7f96b3335904c208ac301581801c683
schweissguth <53831366+schweissguth@users.noreply.github.com>
2025-02-16 14:35:02 -0600

    Update pickstable.js

--- pickstable.js
+++ pickstable.js
@@ -1,88 +1,103 @@
-function init() {
-  pt.innerHTML = null
-  getPlayers()
-}
 
-init()
 
-function getPlayers() {
-  fetch(
-    "https://script.google.com/macros/s/AKfycbyEGYd3C0Zgd-mHbBrjDrdMUSsYvj_7oky1yIuffDrRk8rURlR-gV_IteIJHzznntfO/exec?PLAYERS",
-  )
-    .then(function (players) {
-      return players.json()
-    })
-    .then(function (players) {
-      console.log(players)
-      players = players.filter(function(player) {
-      	return player.ACTIVE
-      })
-      players = players.sort(function (a, b) {
-        return a.PICKORDER - b.PICKORDER
-      })
-      getDrivers(players)
-    })
-}
-
-function getDrivers(players) {
-  fetch(
-    "https://script.google.com/macros/s/AKfycbyEGYd3C0Zgd-mHbBrjDrdMUSsYvj_7oky1yIuffDrRk8rURlR-gV_IteIJHzznntfO/exec?DRIVERS",
-  )
-    .then(function (drivers) {
-      return drivers.json()
-    })
-    .then(function (drivers) {
-      console.log(drivers)
-      drivers = drivers.filter(function(driver) {
-      	return !driver.PLAYERID
-      })
-      drivers.push({
-    "POS": 12.5,
-    "DRIVERID": 0,
-    "FIRSTNAME": "",
-    "LASTNAME": "",
-    "CARNO": "000",
-    "PLAYERID": "",
-    "PLAYERNAME": "",
-    "DRIVERNAME": "--------------"
-})
-drivers.sort(function(a, b) {
-	return a.POS - b.POS
-})
-      makeTable(players, drivers)
-    })
-}
-
-function makeTable(players, drivers) {
-  players.forEach(function (player) {
-    var tr = pt.insertRow()
-    var td0 = tr.insertCell()
-    td0.innerHTML = player.NAME + "<small> (" + Math.round(player.PICKORDER) + ")</small>"
-    var td1 = tr.insertCell()
-    player.PICKS = player.PICKS.split(",")
-    player.PICKS.length = 2
-    player.DRIVERS = player.DRIVERS.split(",")
-    player.DRIVERS.length = 2
-    for (let i = 0; i < 2; i++) {
-      if (player.DRIVERS[i]) {
-        var c = document.createElement("DIV")
-        c.innerText = player.DRIVERS[i]
-        td1.append(c)
-      } else {
-        var select = document.createElement("SELECT")
-        var option = document.createElement("OPTION")
-        option.selected = true
-        select.add(option)
-        drivers.forEach(function (driver) {
-          var opt = document.createElement("OPTION")
-          opt.text = driver.DRIVERNAME
-          opt.value = driver.DRIVERID
-          select.add(opt)
-        })
-      td1.append(select)
-      }
-    	
-    }
-
+function getLiveRace() {
+	fetch("https://cf.nascar.com/cacher/live/live-feed.json").then(function(res) {
+  	return res.json()
+  }).then(function(feeds) {
+  	pt.innerHTML = null
+  	//console.log(feeds)
+    getDrivers(feeds)
   })
 }
+
+function getDrivers(feeds) {
+	fetch("https://script.google.com/macros/s/AKfycbyEGYd3C0Zgd-mHbBrjDrdMUSsYvj_7oky1yIuffDrRk8rURlR-gV_IteIJHzznntfO/exec?DRIVERS").then(function(res) {
+  	return res.json()
+  }).then(function(drivers) {
+  	console.log(drivers)
+    feeds = feeds.vehicles.map(function(feed) {
+    	var find = drivers.find(function(driver) {
+      	return driver.DRIVERID == feed.driver.driver_id
+      }) || {
+      	PLAYERID: null
+      }
+      feed.PLAYERID = find.PLAYERID
+      return feed
+    })
+    console.log(feeds)
+    feeds = feeds.filter(function(feed) {
+    	return feed.PLAYERID
+    })
+    var count = 1
+    feeds.reverse()
+    feeds = feeds.map(function(feed) {
+    	feed.RANK = count++
+      return feed
+    })
+    feeds.reverse()
+    getPlayers(feeds)
+  })
+}
+
+
+getLiveRace()
+
+
+function getPlayers(feeds) {
+	fetch("https://script.google.com/macros/s/AKfycbyEGYd3C0Zgd-mHbBrjDrdMUSsYvj_7oky1yIuffDrRk8rURlR-gV_IteIJHzznntfO/exec?PLAYERS").then(function(players) {
+  	return players.json()
+  }).then(function(players) {
+  	console.log(players)
+    players = players.map(function(player) {
+    	player.RUNORDER = []
+    	player.RANK = []
+    	player.SUM = 0
+      player.PICKS = player.PICKS.split(",")
+      player.PICKS.forEach(function(pick) {
+      	var find = feeds.find(function(feed) {
+        	return feed.driver.driver_id == pick
+        }) || {
+        	running_position: 0,
+          RANK: 0
+        }
+        player.RUNORDER.push(find.running_position)
+        player.RANK.push(find.RANK)
+        player.SUM += find.RANK
+      })
+      return player
+    })
+    makeTable(players)
+  })
+}
+
+
+function makeTable(players) {
+	players.sort(function(a, b) {
+  	return b.SUM - a.SUM
+  })
+  players = players.filter(function(player) {
+  	return player.ACTIVE
+  })
+	console.log(players)
+  tlpicks.innerHTML = null
+  players.forEach(function(player) {
+  	var tr = tlpicks.insertRow()
+    tr.insertCell().innerText = player.NAME
+    player.DRIVERS = player.DRIVERS.split(",")
+    var td1 = tr.insertCell()
+    player.DRIVERS.forEach(function(driver) {
+    	var div = document.createElement("DIV")
+      div.innerText = driver
+      td1.append(div)
+    })
+    var td2 = tr.insertCell()
+    player.RUNORDER.forEach(function(pos) {
+    	var div = document.createElement("DIV")
+      div.innerText = pos
+      td2.append(div)
+    })
+    tr.insertCell().innerHTML = "<b>" + player.SUM + "</b>"
+    
+  })
+}
+
