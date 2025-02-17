9992369177f2e0285ed6d4cf278c2d1490e3b8bf
schweissguth <53831366+schweissguth@users.noreply.github.com>
2025-02-18 04:12:51 +0800

    Update picks.js

--- picks.js
+++ picks.js
@@ -1,62 +1,73 @@
-var DRIVERS = []
-var PLAYERS = []
-
-fetch("https://script.google.com/macros/s/AKfycbyEGYd3C0Zgd-mHbBrjDrdMUSsYvj_7oky1yIuffDrRk8rURlR-gV_IteIJHzznntfO/exec?page=picks").then(function(res) {
-	return res.json()
-}).then(function(res) {
-	console.log(res)
-  DRIVERS = res.standings
-  PLAYERS = res.players
-  makeTable()
-})
-
-
-
-
-function makeTable() {
-	tbody.innerHTML = null
-	DRIVERS.forEach(function(item) {
-  	var tr = tbody.insertRow()
-    
-    var tdimg = tr.insertCell()
-    var img = document.createElement("IMG")
-    img.src = item.IMAGE
-    img.style.width = "50px"
-    tdimg.append(img)
-    
-    tr.insertCell().innerHTML = item.first_name + " " + item.last_name
-    
-    var td = tr.insertCell()
-    
-    var select = document.createElement("SELECT")
-    var option = document.createElement("OPTION")
-    select.append(option)
-    
-    PLAYERS.forEach(function(player) {
-    	var opt = document.createElement("OPTION")
-      opt.text = player.NAME
-      opt.value = player.ID
-      select.append(opt)
+function getStandings() {
+  fetch(
+    "https://script.google.com/macros/s/AKfycbyEGYd3C0Zgd-mHbBrjDrdMUSsYvj_7oky1yIuffDrRk8rURlR-gV_IteIJHzznntfO/exec?STANDINGS",
+  )
+    .then(function (standings) {
+      return standings.json()
     })
-    
-    select.onchange = function() {
-    	fetch("https://script.google.com/macros/s/AKfycbwyx5JZpneWX4L_GyEike2Z8Bqjmgy-8qrNOTVORBSi1ErN6AWO7E8rNM8HIc5bS2nW/exec?PICKS", {
-      	method: "post",
-        body: JSON.stringify({
-          DRIVERID: item.driver_id,
-          PLAYERID: this.value,
+    .then(function (standings) {
+      //console.log(standings)
+      getPlayers(standings)
+    })
+}
+
+getStandings()
+
+function getPlayers(standings) {
+  fetch(
+    "https://script.google.com/macros/s/AKfycbyEGYd3C0Zgd-mHbBrjDrdMUSsYvj_7oky1yIuffDrRk8rURlR-gV_IteIJHzznntfO/exec?PLAYERS",
+  )
+    .then(function (players) {
+      return players.json()
+    })
+    .then(function (players) {
+      //console.log(players)
+      standings.forEach(function (standing) {
+        var tr = table.insertRow()
+        var td0 = tr.insertCell().id = standing.driver_id
+        tr.insertCell().innerText =
+          standing.first_name + " " + standing.last_name
+        var td = tr.insertCell()
+        var select = document.createElement("SELECT")
+        var option = document.createElement("OPTION")
+        select.add(option)
+        players.forEach(function (player) {
+          var opt = document.createElement("OPTION")
+          opt.text = player.NAME
+          opt.value = player.ID
+          select.add(opt)
         })
+        select.onchange = function() {
+        	fetch("https://script.google.com/macros/s/AKfycbyEGYd3C0Zgd-mHbBrjDrdMUSsYvj_7oky1yIuffDrRk8rURlR-gV_IteIJHzznntfO/exec?PICKS", {
+          	method: "post",
+            body: JSON.stringify({
+            	DRIVERID: standing.driver_id,
+              PLAYERID: this.value
+            })
+          })
+        }
+        select.value = standing.PLAYERID
+        td.append(select)
       })
-    }
-    select.value = item.PLAYERID
-    td.append(select)
-    
-    
- 
-    
-  })
-var tr = tbody.insertRow(13)
-  var td = tr.insertCell()
-	td.setAttribute("colspan", "3")
-	td.innerHTML = "<hr>"
+      var td13 = table.insertRow(13).insertCell()
+      td13.setAttribute("colspan", 3)
+      td13.innerHTML = "<hr>"
+      getImages()
+    })
+}
+
+function getImages() {
+  fetch("https://cf.nascar.com/cacher/drivers.json")
+    .then(function (res) {
+      return res.json()
+    })
+    .then(function (images) {
+      images.response.forEach(function (image) {
+        try {
+          var td = document.getElementById(image.Nascar_Driver_ID)
+          td.innerHTML =
+            "<img src='" + image.Image_Small + "' width='50' />"
+        } catch (err) {}
+      })
+    })
 }
