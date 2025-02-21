1bb5af8d561488dae5923486de92ae2110761648
schweissguth <53831366+schweissguth@users.noreply.github.com>
2025-02-21 13:23:05 +0800

    Update library.js

--- library.js
+++ library.js
@@ -1,19 +1,11 @@
-function gviz(url) {
-  fetch(url)
-    .then(function (res) {
-      return res.json()
-    })
-    .then(function (res) {
-      res = res.values
-      console.log(res)
-      var header = res.shift()
-      var array = res.map(function (row) {
-        return row.reduce(function (acc, cur, i) {
-          acc[header[i]] = cur
-          return acc
-        }, {})
-      })
-      console.log(array)
-      return array
-    })
+Array.prototype.makeObj = function () {
+  var header = this.shift()
+  var json = this.map(function (row) {
+    return row.reduce(function (acc, cur, i) {
+      acc[header[i]] = cur
+      return acc
+    }, {})
+  })
+  console.log(json)
+  return json
 }
