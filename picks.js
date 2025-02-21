0b80008d8b3a3192549e3b60163630ba3b32ade7
schweissguth <53831366+schweissguth@users.noreply.github.com>
2025-02-21 15:37:34 +0800

    Update picks.js

--- picks.js
+++ picks.js
@@ -1,65 +1,65 @@
-var cutoff = 13
-  function getStandings() {
-  fetch(
-    "https://script.google.com/macros/s/AKfycbyEGYd3C0Zgd-mHbBrjDrdMUSsYvj_7oky1yIuffDrRk8rURlR-gV_IteIJHzznntfO/exec?STANDINGS",
-  )
-    .then(function (standings) {
-      return standings.json()
-    })
-    .then(function (standings) {
-      //console.log(standings)
-      getPlayers(standings)
-    })
-}
-
-getStandings()
+fetch(
+  "https://sheets.googleapis.com/v4/spreadsheets/1jwadkJYYfBmf-SbjUokDV0S_yzC7gD39-jHxVatJfLU/values/STANDINGS?key=AIzaSyDIEdL4EcBenrWDkh03oFYmFvHT_VNH3AI",
+)
+  .then(function (res) {
+    return res.json()
+  })
+  .then(function (standings) {
+    standings = standings.values.makeObj()
+    getPlayers(standings)
+  })
 
 function getPlayers(standings) {
   fetch(
-    "https://script.google.com/macros/s/AKfycbyEGYd3C0Zgd-mHbBrjDrdMUSsYvj_7oky1yIuffDrRk8rURlR-gV_IteIJHzznntfO/exec?PLAYERS",
+    "https://sheets.googleapis.com/v4/spreadsheets/1jwadkJYYfBmf-SbjUokDV0S_yzC7gD39-jHxVatJfLU/values/PLAYERS?key=AIzaSyDIEdL4EcBenrWDkh03oFYmFvHT_VNH3AI",
   )
-    .then(function (players) {
-      return players.json()
+    .then(function (res) {
+      return res.json()
     })
     .then(function (players) {
+      players = players.values.makeObj()
       console.log(players)
-      table.innerHTML = null
       players = players.filter(function(player) {
-      	return player.ACTIVE
+      	return player.ACTIVE == "TRUE"
+      })
+      players.sort(function(a, b) {
+      	return a.PICKORDER - b.PICKORDER
       })
       standings.forEach(function (standing) {
-        cutoff = standing.CUTOFF
         var tr = table.insertRow()
-        var td0 = tr.insertCell().id = standing.driver_id
-        tr.insertCell().innerText =
-          standing.first_name + " " + standing.last_name
+        tr.insertCell().id = standing.driver_id
+        tr.insertCell().innerText = standing.DRIVERNAME
         var td = tr.insertCell()
         var select = document.createElement("SELECT")
         var option = document.createElement("OPTION")
         select.add(option)
+        td.append(select)
         players.forEach(function (player) {
           var opt = document.createElement("OPTION")
           opt.text = player.PLAYERNAME
           opt.value = player.PLAYERID
           select.add(opt)
         })
-        select.onchange = function() {
-        	fetch("https://script.google.com/macros/s/AKfycbyEGYd3C0Zgd-mHbBrjDrdMUSsYvj_7oky1yIuffDrRk8rURlR-gV_IteIJHzznntfO/exec?PICKS", {
-          	method: "post",
-            body: JSON.stringify({
-            	DRIVERID: standing.driver_id,
-              PLAYERID: this.value,
-              DRIVERNAME: standing.first_name + " " + standing.last_name
-            })
-          })
+        select.onchange = function () {
+          fetch(
+            "https://script.google.com/macros/s/AKfycbyEGYd3C0Zgd-mHbBrjDrdMUSsYvj_7oky1yIuffDrRk8rURlR-gV_IteIJHzznntfO/exec?PICKS",
+            {
+              method: "post",
+              body: JSON.stringify({
+                DRIVERID: standing.driver_id,
+                PLAYERID: this.value,
+                DRIVERNAME: standing.first_name + " " + standing.last_name,
+              }),
+            },
+          )
         }
         select.value = standing.PLAYERID
-        td.append(select)
       })
-      var td13 = table.insertRow(cutoff).insertCell()
-      td13.setAttribute("colspan", 3)
-      td13.innerHTML = "<hr>"
       getImages()
+      var tr = table.insertRow(standings[0].CUTOFF)
+      var td = tr.insertCell()
+      td.setAttribute("colspan", 3)
+      td.innerHTML = "<hr>"
     })
 }
 
@@ -69,11 +69,12 @@ function getImages() {
       return res.json()
     })
     .then(function (images) {
-      images.response.forEach(function (image) {
+      console.log(images)
+      images = images.response
+      images.forEach(function (image) {
         try {
           var td = document.getElementById(image.Nascar_Driver_ID)
-          td.innerHTML =
-            "<img src='" + image.Image_Small + "' width='50' />"
+          td.innerHTML = "<img src='" + image.Badge_Image + "' width='50' />"
         } catch (err) {}
       })
     })
