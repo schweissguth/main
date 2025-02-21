de12b79b61eadcd746628ffa299c88dab35e0551
schweissguth <53831366+schweissguth@users.noreply.github.com>
2025-02-21 15:58:37 +0800

    Update pickstable.js

--- pickstable.js
+++ pickstable.js
@@ -1,15 +1,6 @@
-fetch(
-  "https://sheets.googleapis.com/v4/spreadsheets/1jwadkJYYfBmf-SbjUokDV0S_yzC7gD39-jHxVatJfLU/values/STANDINGS?key=AIzaSyDIEdL4EcBenrWDkh03oFYmFvHT_VNH3AI",
-)
-  .then(function (res) {
-    return res.json()
-  })
-  .then(function (standings) {
-    standings = standings.values.makeObj()
-    getPlayers(standings)
-  })
 
-function getPlayers(standings) {
+
+function getPlayers() {
   fetch(
     "https://sheets.googleapis.com/v4/spreadsheets/1jwadkJYYfBmf-SbjUokDV0S_yzC7gD39-jHxVatJfLU/values/PLAYERS?key=AIzaSyDIEdL4EcBenrWDkh03oFYmFvHT_VNH3AI",
   )
@@ -25,57 +16,19 @@ function getPlayers(standings) {
       players.sort(function(a, b) {
       	return a.PICKORDER - b.PICKORDER
       })
-      standings.forEach(function (standing) {
-        var tr = table.insertRow()
-        tr.insertCell().id = standing.driver_id
-        tr.insertCell().innerText = standing.DRIVERNAME
+      players.forEach(function (player) {
+        var tr = pt.insertRow()
+        tr.insertCell().innerHTML = "<b>" + player.PLAYERNAME + "</b>"
+        + " <small>(" + Math.round(player.PICKORDER) + ")</small>"
+        var div1 = document.createElement("DIV")
+        var div2 = document.createElement("DIV")
+        div1.innerText = player.DRIVERNAME1 || null
+        div2.innerText = player.DRIVERNAME2 || null
         var td = tr.insertCell()
-        var select = document.createElement("SELECT")
-        var option = document.createElement("OPTION")
-        select.add(option)
-        td.append(select)
-        players.forEach(function (player) {
-          var opt = document.createElement("OPTION")
-          opt.text = player.PLAYERNAME
-          opt.value = player.PLAYERID
-          select.add(opt)
-        })
-        select.onchange = function () {
-          fetch(
-            "https://script.google.com/macros/s/AKfycbyEGYd3C0Zgd-mHbBrjDrdMUSsYvj_7oky1yIuffDrRk8rURlR-gV_IteIJHzznntfO/exec?PICKS",
-            {
-              method: "post",
-              body: JSON.stringify({
-                DRIVERID: standing.driver_id,
-                PLAYERID: this.value,
-                DRIVERNAME: standing.first_name + " " + standing.last_name,
-              }),
-            },
-          )
-        }
-        select.value = standing.PLAYERID
+        td.append(div1)
+        td.append(div2)
       })
-      getImages()
-      var tr = table.insertRow(standings[0].CUTOFF)
-      var td = tr.insertCell()
-      td.setAttribute("colspan", 3)
-      td.innerHTML = "<hr>"
     })
 }
 
-function getImages() {
-  fetch("https://cf.nascar.com/cacher/drivers.json")
-    .then(function (res) {
-      return res.json()
-    })
-    .then(function (images) {
-      console.log(images)
-      images = images.response
-      images.forEach(function (image) {
-        try {
-          var td = document.getElementById(image.Nascar_Driver_ID)
-          td.innerHTML = "<img src='" + image.Badge_Image + "' width='50' />"
-        } catch (err) {}
-      })
-    })
-}
+getPlayers()
