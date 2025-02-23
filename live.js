1aad6adffb74a6b6f7f8b7ecd40ede05c6175897
schweissguth <53831366+schweissguth@users.noreply.github.com>
2025-02-23 05:54:30 -0600

    Update live.js

--- live.js
+++ live.js
@@ -1,43 +1,53 @@
-
-
 function getLiveRace() {
-	fetch("https://cf.nascar.com/cacher/live/live-feed.json").then(function(res) {
-  	return res.json()
-  }).then(function(feeds) {
-  	tlive.innerHTML = null
-  	console.log(feeds)
-    getLiveDrivers(feeds)
-  })
+  fetch("https://cf.nascar.com/cacher/live/live-feed.json")
+    .then(function (res) {
+      return res.json()
+    })
+    .then(function (feeds) {
+      tlive.innerHTML = null
+      console.log(feeds)
+      getLiveDrivers(feeds)
+    })
 }
 
 getLiveRace()
 
-
 function getLiveDrivers(feeds) {
-	fetch("https://script.google.com/macros/s/AKfycbyEGYd3C0Zgd-mHbBrjDrdMUSsYvj_7oky1yIuffDrRk8rURlR-gV_IteIJHzznntfO/exec?DRIVERS").then(function(drivers) {
-  	return drivers.json()
-  }).then(function(drivers) {
-  	console.log(drivers)
-    feeds.vehicles = feeds.vehicles.map(function(feed) {
-    	var find = drivers.find(function(driver) {
-      	return feed.driver.driver_id == driver.DRIVERID
-      }) || {
-      	PLAYERNAME: ""
-      }
-      feed.PLAYERNAME = find.PLAYERNAME
-      return feed
+  fetch(
+    "https://sheets.googleapis.com/v4/spreadsheets/1jwadkJYYfBmf-SbjUokDV0S_yzC7gD39-jHxVatJfLU/values/DRIVERS?key=AIzaSyDIEdL4EcBenrWDkh03oFYmFvHT_VNH3AI",
+  )
+    .then(function (drivers) {
+      return drivers.json()
     })
-    makeLiveTable(feeds)
-  })
+    .then(function (drivers) {
+      drivers = drivers.values.makeObj()
+      console.log(drivers)
+      feeds.vehicles = feeds.vehicles.map(function(feed) {
+      	feed.PLAYERNAME = ""
+        return feed
+      })
+      drivers.forEach(function (driver) {
+        var find = feeds.vehicles.find(function (feed) {
+          return feed.driver.driver_id == driver.driver_id
+        })
+        if (find) {
+          find.PLAYERNAME = driver.PLAYERNAME
+        }
+      })
+      makeLiveTable(feeds)
+    })
 }
 
-
 function makeLiveTable(feeds) {
-	feeds.vehicles.forEach(function(feed) {
-  	var tr = tlive.insertRow()
+  feeds.vehicles.forEach(function (feed) {
+    var tr = tlive.insertRow()
     tr.insertCell().innerText = feed.running_position + " "
-    tr.insertCell().innerHTML = feed.driver.full_name + " " + "<i><small>-" + Math.round(feed.delta * 100)/100 + "</small></i>"
+    tr.insertCell().innerHTML =
+      feed.driver.full_name +
+      " " +
+      "<i><small>-" +
+      Math.round(feed.delta * 100) / 100 +
+      "</small></i>"
     tr.insertCell().innerHTML = "<b>" + feed.PLAYERNAME + "</b>"
   })
-  
 }
