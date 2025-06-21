c3741ee2c62a4623ab7962118ff23fcc2a4fd3ca
schweissguth <53831366+schweissguth@users.noreply.github.com>
2025-06-21 15:28:44 -0500

    Update pickstable.js

--- pickstable.js
+++ pickstable.js
@@ -5,9 +5,8 @@ getPickorder().then(function(pickers) {
     let tr = pickstable.insertRow()
     let td = tr.insertCell()
     let a = document.createElement("A")
-    a.innerHTML = picker.playername + " <small>(" + picker.TOTAL + ")</small>"
+    a.innerHTML = "<b>" + picker.playername + </b> + " <small>(" + picker.TOTAL + ")</small>"
     a.href = "javascript:"
-    a.style.fontWeight = "bold"
     a.onclick = function() {
       if (confirm("Alert " + picker.playername + " to pick?")) {
         fetch("https://script.google.com/macros/s/AKfycby3oJXHAF7Nx5rEXCd2Fy3mTdkRKHAkj9L-3m4cz-JMZet2Ug4DELuccc0Re4vfoYk/exec?" + encodeURIComponent(picker.cell))
