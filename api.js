ebfca4230148bd89f2b0713d3050b40efc5a35c9
schweissguth <53831366+schweissguth@users.noreply.github.com>
2026-02-25 19:38:12 -0600

    Add files via upload

--- api.js
+++ api.js
@@ -1,5 +1,296 @@
-fetch("https://cf.nascar.com/cacher/2026/1/race_list_basic.json").then(function(res) {
-  return res.json()
-}).then(function(res) {
-  return res
-})
+async function calcScores() {
+  const leaderboard = await getLeaderboard();
+  const pickorder = await getPickOrder(leaderboard.race_id);
+
+  const ARR = [];
+  const dobj = { id: 0, name: '', pos: 0, pts: 0 };
+  pickorder.forEach(function (each) {
+    ARR.push({
+      id: each.playerid,
+      name: '',
+      group: Number(each.groupid),
+      drivers: [structuredClone(dobj), structuredClone(dobj)],
+      total: 0,
+      winnings: 0,
+      raceid: leaderboard.race_id
+    });
+  });
+
+  const players = await getPlayers();
+  console.log(players);
+  ARR.forEach(function (each) {
+    each.name = players[each.id];
+  });
+
+  const picks = await getPicks(leaderboard.race_id);
+  const pickso = {};
+  picks.forEach(function (each) {
+    pickso[each.playerid] = [];
+  });
+  picks.forEach(function (pick) {
+    pickso[pick.playerid].push(pick.driverid);
+  });
+  console.log('pickso', pickso);
+
+  const leaderboardo = {};
+  leaderboard.vehicles.forEach(function (driver) {
+    leaderboardo[driver.driver.driver_id] = driver;
+  });
+
+  ARR.forEach(function (each) {
+    each.drivers.forEach(function (each2, i) {
+      each2.id = pickso[each.id][i];
+      each2.name = leaderboardo[each2.id].driver.full_name;
+      each2.pos = leaderboardo[each2.id].running_position;
+    });
+  });
+
+  const scores = [];
+  const scoreso = [];
+  ARR.forEach(function (each) {
+    scores[each.group] = [];
+    scoreso[each.group] = {};
+  });
+  ARR.forEach(function (each, i) {
+    each.drivers.forEach(function (item) {
+      scores[each.group].push(item);
+      scoreso[each.group][item.id] = 0;
+    });
+  });
+  scores.forEach(function (each) {
+    let counter = 1;
+    each.sort(function (a, b) {
+      return b.pos - a.pos;
+    });
+    each.forEach(function (item) {
+      item.pts = counter++;
+      if (item.pos == 1) item.pts = item.pts + 2;
+    });
+  });
+
+  console.log('ARR', ARR);
+
+  ARR.forEach(function (each) {
+    each.drivers.forEach(function (item) {
+      each.total += item.pts;
+    });
+  });
+
+  ARR.sort(function (a, b) {
+    return b.total - a.total;
+  });
+
+  return ARR;
+}
+
+
+async function makePickOrder() {
+  const raceid = await getLeaderboard().then(function (res) {
+    return res.race_id;
+  });
+  const scores = await getScores(raceid);
+  console.log('scores', scores);
+
+  const OBJ = {};
+  scores.forEach(function (each) {
+    OBJ[each.playerid] = 0;
+  });
+  scores.forEach(function (each) {
+    OBJ[each.playerid] += each.score;
+  });
+  console.log('OBJ', OBJ);
+
+  const season = await getSeason();
+  console.log('season', season);
+  const seasono = {};
+  season.forEach(function (each) {
+    seasono[each.id] = each.sum;
+  });
+
+  const TEMP = Object.entries(OBJ);
+  TEMP.forEach(function (each) {
+    each[1] = each[1] + seasono[each[0]] / 10000;
+  });
+
+  TEMP.sort(function (a, b) {
+    return a[1] - b[1];
+  });
+
+  TEMP.forEach(function (each, i) {
+    each[2] = i % 2;
+  });
+  console.log(TEMP);
+
+  const ARR = [];
+  TEMP.forEach(function (each) {
+    ARR[each[2]] = [];
+  });
+
+  const players = await getPlayers();
+  TEMP.forEach(function (each) {
+    ARR[each[2]].push({
+      id: each[0],
+      name: players[each[0]],
+      rank: each[1],
+    });
+  });
+
+  console.log('ARR', ARR);
+  return ARR;
+}
+
+async function getPickSummary() {
+  const schedule = await getSchedule()
+  const race = schedule.find(x => !x.winner_driver_id)
+  console.log("race", race)
+  const pickorder = await getPickOrder(race.race_id)
+  console.log("pickorder", pickorder)
+  let ARR = []
+  const groups = await getGroups()
+  console.log("groups", groups)
+  groups.forEach(function (each, i) {
+    ARR.push({
+      name: each.name,
+      players: [],
+      group: i
+    })
+  })
+  const players = await getPlayers()
+  pickorder.forEach(function (each) {
+    ARR[each.groupid].players.push({
+      id: each.playerid,
+      name: players[each.playerid],
+      score: Math.floor(each.rank),
+      drivers: []
+    })
+  })
+  const picks = await getPicks(race.race_id)
+  const pickso = {}
+  console.log("pickso", pickso)
+  picks.forEach(function (each) {
+    pickso[each.playerid] = []
+  })
+  const drivers = await getDrivers()
+  const driverso = {}
+  drivers.forEach(function (each) {
+    driverso[each.driver_id] = each.driver_name
+  })
+  picks.forEach(function (each) {
+    pickso[each.playerid].push({
+      id: each.driverid,
+      name: driverso[each.driverid]
+    })
+  })
+  ARR.forEach(function (each) {
+    each.players.forEach(function (item) {
+      item.drivers = pickso[item.id] || []
+    })
+  })
+  console.log("ARR", ARR)
+  return ARR
+}
+
+
+
+
+async function showLeaderboard() {
+  const leaders = await getLeaderboard()
+  let ARR = []
+  let pobj = { id: "", name: "", score: 0 }
+  leaders.vehicles.forEach(function (each) {
+    ARR.push({
+      id: each.driver.driver_id,
+      name: each.driver.full_name,
+      pos: each.running_position,
+      players: [structuredClone(pobj), structuredClone(pobj)]
+    })
+  })
+  const picks = await getPicks(leaders.race_id)
+  console.log("PICKS", picks)
+  let pickso = {}
+  picks.forEach(function (each) {
+    pickso[each.driverid] = []
+  })
+  picks.forEach(function (each) {
+    pickso[each.driverid][each.groupid] = each.playerid
+  })
+  const players = await getPlayers()
+  ARR.forEach(function (each) {
+    each.players.forEach(function (item, i) {
+      item.id = pickso[each.id]?.[i]
+      item.name = players[item.id] || ""
+    })
+
+
+  })
+
+  ARR.reverse();
+  [1, 2].forEach(function (_, i) {
+    let counter = 1
+    ARR.forEach(function (item) {
+      if (item.players[i]?.id) {
+        item.players[i].score = counter++
+      }
+      if (item.pos == 1) {
+        item.players[i].score++
+        item.players[i].score++
+      }
+    })
+  })
+  ARR.reverse()
+  return ARR
+}
+
+
+
+async function makePicks(x) {
+  const schedule = await getSchedule()
+  const race = schedule.find(x => !x.winner_driver_id)
+  const pickorder = await getPickOrder(race.race_id)
+  console.log("pickorder", pickorder)
+  const groupid = pickorder.find(each => each.playerid == x).groupid
+  const drivers = await getDrivers()
+  let OBJ = {
+    group: groupid,
+    id: race.race_id,
+    drivers: []
+  }
+  drivers.forEach(function(each) {
+    OBJ.drivers.push({
+      id: each.driver_id,
+      name: each.driver_name,
+      badge: each.car_no,
+      player: {
+        id: "",
+        name: ""
+      },
+      raceid: race.race_id,
+    })
+  })
+
+  let picks = await getPicks(race.race_id)
+  picks = picks.filter(function(each) {
+    return each.groupid == groupid
+  })
+  const pickso = {}
+  picks.forEach(function(each) {
+    pickso[each.driverid] = each.playerid
+  })
+  console.log("pickso", pickso)
+
+  const players = await getPlayers()
+
+  OBJ.drivers.forEach(function(each) {
+    each.player.id = pickso[each.id] || ""
+    each.player.name = players[pickso[each.id]] || ""
+  })
+
+
+
+
+
+
+
+  return OBJ
+}
+
--- server.js
+++ server.js
@@ -1,233 +1,116 @@
-
-    async function picksObj(x) {
-  const picks = await getPicks(x)
-  let picksobj = []
-  picks.forEach(function(pick) {
-    picksobj[pick.GROUPID] = {}
-  })
-  picks.forEach(function(pick) {
-    picksobj[pick.GROUPID][pick.DRIVERID] = pick.PLAYERID
-  })
-  return picksobj
-}
-
-async function getPickSummary(x) {
-  const pickorder = await pickorderObj(5596)
-  const playersobj = await playersObj()
-  const playerpicksobj = await playerpicksObj(5596)
-  const driversobj = await driversObj()
-  console.log("driversobj", driversobj)
-
-  let picksummary = []
-  pickorder.forEach(function(group, g) {
-    picksummary[g] = []
-  })
-  pickorder.forEach(function(group, g) {
-    group.forEach(function(playerid) {
-      let arr = []
-      playerpicksobj[playerid].forEach(function(driverid) {
-        arr.push(driversobj[driverid].driver_name)
-      })
-      picksummary[g].push({
-        id: playerid,
-        name: playersobj[playerid].NAME,
-        driverids: playerpicksobj[playerid],
-        drivernames: arr
-      })
-    })
-  })
-  return picksummary
-}
-
-Array.prototype.makeObj = function() {
-  let header = this.shift()
-  return this.map(function(each) {
-    let obj = {}
+Array.prototype.makeObj = function () {
+  let header = this.shift();
+  return this.map(function (each) {
+    let obj = {};
     for (let i = 0; i < header.length; i++) {
-      obj[header[i]] = each[i] || null
+      obj[header[i].toLowerCase()] = each[i] || null;
     }
-    return obj
-  })
-}
-
-
-
-
-function currentResults() {
-  return fetch("https://cf.nascar.com/data/cacher/production/live/current-results.json").then(
-    function (res) {
-      return res.json()
-    },
-  )
-}
-
-function getOps() {
-  return fetch("https://cf.nascar.com/live-ops/live-ops.json").then(
-    function (res) {
-      return res.json()
-    },
-  )
-}
-
-function nextRace() {
-  return getSchedule().then(function(res) {
-    console.log(res)
-    res = res.filter(function(race) {
-      return race.race_type_id == 1
-    })
-    res = res.find(function(race) {
-    return !race.winner_driver_id
-  }).race_id
-  return res
-  })
-}
-
-
-function prevRace() {
-  return getSchedule().then(function(res) {
-    res = res.filter(function(race) {
-      return race.race_type_id == 1
-    })
-    res.reverse()
-    res = res.find(function(race) {
-    return race.winner_driver_id
-  }).race_id
-  return res
-  })
-}
-
-
-function getPicks(x) {
-  return fetch(
-    "https://sheets.googleapis.com/v4/spreadsheets/1jwadkJYYfBmf-SbjUokDV0S_yzC7gD39-jHxVatJfLU/values/PICKS?key=AIzaSyDIEdL4EcBenrWDkh03oFYmFvHT_VNH3AI",
-  )
-    .then(function (res) {
-      return res.json()
-    })
-    .then(function (res) {
-      if (x) {
-        return res.values.makeObj().filter(function(filter) {
-          return filter.RACEID == x
-        })
-      } else {
-        return res.values.makeObj()
-      }
-    })
-}
-
-
-function getChase() {
-  return fetch(
-    "https://sheets.googleapis.com/v4/spreadsheets/1jwadkJYYfBmf-SbjUokDV0S_yzC7gD39-jHxVatJfLU/values/CHASE?key=AIzaSyDIEdL4EcBenrWDkh03oFYmFvHT_VNH3AI",
-  )
-    .then(function (res) {
-      return res.json()
-    })
-    .then(function (res) {
-      return res.values.makeObj()
-    })
-}
+    return obj;
+  });
+};
 
 function getData(x) {
   return fetch(
     'https://sheets.googleapis.com/v4/spreadsheets/1jwadkJYYfBmf-SbjUokDV0S_yzC7gD39-jHxVatJfLU/values/' +
-      x +
-      '?key=AIzaSyDIEdL4EcBenrWDkh03oFYmFvHT_VNH3AI',
+    x +
+    '?key=AIzaSyDIEdL4EcBenrWDkh03oFYmFvHT_VNH3AI',
   )
     .then(function (res) {
-      return res.json()
+      return res.json();
     })
     .then(function (res) {
-      return res.values.makeObj()
-    })
+      return res.values.makeObj();
+    });
 }
 
-function getScores(raceid) {
-  return getData('SCORES').then(function (res) {
-    return res.filter(function(filter) {
-      return filter.RACEID == raceid
-    })
-  })
+function getLeaderboard() {
+  return fetch('https://cf.nascar.com/cacher/live/live-feed.json').then(
+    function (res) {
+      return res.json();
+    },
+  );
 }
 
-function getScores(raceid) {
-  return getData('SCORES').then(function (res) {
-    return res.filter(function (filter) {
-      return filter.RACEID == raceid
-    })
-  })
+function getPicks(x) {
+  return getData('PICKS').then(function (res) {
+    res = res.filter(function (pick) {
+      return pick.raceid == x;
+    });
+    return res.filter(function (filter, f) {
+      return (
+        res.findLastIndex(function (find) {
+          return (
+            find.driverid == filter.driverid && find.groupid == filter.groupid
+          );
+        }) == f
+      );
+    });
+  });
+}
+
+function getPickOrder(x) {
+  return getData('PICKORDER').then(function (res) {
+    return res.filter(function (pick) {
+      return pick.raceid == x;
+    });
+  });
 }
 
 function getPlayers() {
   return getData('PLAYERS').then(function (res) {
-    return res.filter(function (filter) {
-      return filter.ACTIVE == "TRUE"
-    })
-  })
+    res = res.filter(function (pick) {
+      return pick.active == 'TRUE';
+    });
+    let obj = {};
+    res.forEach(function (each) {
+      obj[each.playerid] = each.playername;
+    });
+    return obj;
+  });
 }
 
-function playersObj() {
-  return getPlayers().then(function(res) {
-    let obj = {}
-    res.forEach(function(player) {
-      obj[player.ID] = player
-    })
-    return obj
-  })
+function getScores(x) {
+  return getData('SCORES').then(function (res) {
+    res = res.filter(function (each) {
+      return each.raceid == x;
+    });
+    res.forEach(function (each) {
+      each.groupid = Number(each.groupid);
+      each.pos = Number(each.pos);
+      each.score = Number(each.score);
+      each.winnings = Number(each.winnings);
+    });
+    return res;
+  });
 }
-
-async function groupObj() {
-  const schedule = await getSchedule()
-  const pickorder = await getPickOrder()
-  
-  let groupobj = {}
-  schedule.forEach(function (race) {
-    groupobj[race.race_id] = {}
-  })
-  pickorder.forEach(function (picker) {
-    groupobj[picker.RACEID][picker.PLAYERID] = {
-      group: picker.GROUPID,
-      rank: picker.RANK,
-    }
-  })
-  return groupobj
-}
-
-async function picksObj(x) {
-  const picks = await getPicks(x)
-  let picksobj = []
-  picks.forEach(function(pick) {
-    picksobj[pick.GROUPID] = {}
-  })
-  picks.forEach(function(pick) {
-    picksobj[pick.GROUPID][pick.DRIVERID] = pick.PLAYERID
-  })
-  return picksobj
-}
-
-
 
 function getSeason() {
-  return getData('SEASON')
+  return getData('SEASON').then(function (res) {
+    res.forEach(function (each) {
+      each.sum = Number(each.sum);
+    });
+    return res;
+  });
 }
 
 function getSchedule() {
-  return fetch("https://cf.nascar.com/cacher/2026/1/race_list_basic.json").then(
+  return fetch('https://cf.nascar.com/cacher/2026/1/race_list_basic.json').then(
     function (res) {
-      return res.json()
+      return res.json();
     },
-  )
+  );
 }
 
-function getLiveFeed() {
-  return fetch("https://cf.nascar.com/cacher/live/live-feed.json").then(
-    function (res) {
-      return res.json()
-    },
-  )
+function getGroups() {
+  return getData('GROUPS').then(function (res) {
+    res.forEach(function (each) {
+      each.gid = Number(each.gid);
+    });
+    return res;
+  });
 }
 
-function getStandings() {
+function getDrivers() {
   return fetch("https://cf.nascar.com/cacher/2026/1/points-feed.json").then(
     function (res) {
       return res.json()
@@ -235,23 +118,6 @@ function getStandings() {
   )
 }
 
-function getResults(x) {
-  return fetch(
-    "https://cf.nascar.com/data/cacher/production/2026/1/" +
-      x +
-      "/raceResults.json",
-  ).then(function (res) {
-    return res.json()
-  })
-}
-
-function getFlag() {
-  return fetch("https://cf.nascar.com/cacher/live/live-feed.json").then(
-    function (res) {
-      return res.json()
-    },
-  )
-}
 
 function getLapNotes() {
   return getLiveFeed()
@@ -260,7 +126,7 @@ function getLapNotes() {
     })
     .then(function (raceid) {
       return fetch(
-        "https://cf.nascar.com/cacher/2026/1/" + raceid + "/lap-notes.json",
+        "",
       )
     })
     .then(function (res) {
@@ -271,355 +137,21 @@ function getLapNotes() {
     })
 }
 
-function getStages(x) {
-  return fetch(
-    "https://cf.nascar.com/cacher/2026/1/" + x + "/live-stage-points.json",
-  ).then(function (res) {
-    return res.json()
-  })
-}
 
-function getPoints(x) {
-  return fetch(
-    "https://cf.nascar.com/live/feeds/series_1/" + x + "/live_points.json",
-  ).then(function (res) {
-    return res.json()
-  })
-}
-
-function getGroups() {
-  return fetch(
-    "https://sheets.googleapis.com/v4/spreadsheets/1jwadkJYYfBmf-SbjUokDV0S_yzC7gD39-jHxVatJfLU/values/GROUPS?key=AIzaSyDIEdL4EcBenrWDkh03oFYmFvHT_VNH3AI",
-  )
-    .then(function (res) {
+function getLapNotes(x) {
+  return fetch("https://cf.nascar.com/cacher/2026/1/" + x + "/lap-notes.json").then(
+    function (res) {
       return res.json()
-    })
-    .then(function (res) {
-      return res.values.makeObj()
-    })
-}
-
-
-
-function getPickOrder(x) {
-  return fetch(
-    'https://sheets.googleapis.com/v4/spreadsheets/1jwadkJYYfBmf-SbjUokDV0S_yzC7gD39-jHxVatJfLU/values/PICKORDER?key=AIzaSyDIEdL4EcBenrWDkh03oFYmFvHT_VNH3AI',
-  )
-    .then(function (res) {
-      return res.json()
-    })
-    .then(function (res) {
-      if (x) {
-          res = res.values.makeObj()
-          res = res.filter(function (filter) {
-            return filter.RACEID == x
-          })
-          return res.sort(function(a, b) {
-            return parseFloat(a.RANK) - parseFloat(b.RANK)
-          })
-      } else {
-        return res.values.makeObj()
-      }
+    }).then(function(res) {
+      res = res.laps
+      res = Object.entries(res).reverse()
+      res = res.map(function(each) {
+        let str = ""
+        each[1].forEach(function(item) {
+          str += item.Note + ". "
+        })
+        return [each[0], str]
+      })
+      return res
     })
 }
-
-
-
-async function getRunningOrder() {
-  const race = await getLiveFeed()
-  const picks = await driverpicksobj(race.race_id)
-  const players = await playersobj()
-
-  let MAP = new Map()
-  race.vehicles.forEach(function(driver) {
-    MAP.set(driver.driver.driver_id, {
-      id: driver.driver.driver_id,
-      name: driver.driver.full_name,
-      pos: driver.running_position,
-      delta: driver.delta
-    })
-  })
-
-  MAP.forEach(function(driver) {
-    driver.groups = new Array(picks.length)
-  })
-
-  picks.forEach(function(pick, p) {
-    MAP.forEach(function(driver) {
-      driver.groups[p] = {
-        id: picks[p][driver.id] || null,
-        name: players[picks[p][driver.id]]?.NAME,
-        pts: 0
-      }
-    })
-  })
-
-  MAP = new Map([...MAP].reverse())
-  picks.forEach(function(pick, p) {
-    let counter = 0
-    MAP.forEach(function(driver) {
-      let loc = driver.groups[p]
-      if (loc.id) loc.pts = ++counter
-    })
-  })
-  MAP = new Map([...MAP].reverse())
-  return [...MAP.values()]
-}
-
-async function getPlayerResults() {
-  let runningorder = await getRunningOrder()
-  let players = await getPlayers()
-  let pickorder = await getPickOrder()
-  let groups = await getGroups()
-
-  let playersobj = {}
-  players.forEach(function (player) {
-    playersobj[player.PLAYERID] = player.PLAYERNAME
-  })
-
-  let OBJ = []
-  groups.forEach(function () {
-    OBJ.push({})
-  })
-
-  pickorder.forEach(function (picker, p) {
-    OBJ[picker.GROUPID][picker.PLAYERID] = {
-      id: picker.PLAYERID,
-      name: playersobj[picker.PLAYERID],
-      order: p,
-      driverids: [],
-      drivernames: [],
-      scores: [],
-      total: 0,
-      pos: [],
-      winnings: 0,
-    }
-  })
-
-  runningorder.forEach(function (group, g) {
-    group.drivers.forEach(function (driver) {
-      if (driver.playerid) {
-        let obj = OBJ[g][driver.playerid]
-        obj.driverids.push(driver.id)
-        obj.drivernames.push(driver.name)
-        obj.scores.push(driver.score)
-        obj.total += driver.score
-        obj.pos.push(driver.pos)
-      }
-    })
-  })
-
-  OBJ.forEach(function (group, g) {
-    let max = 0
-    let len = 0
-    for (let i in group) {
-      let total = group[i].total
-      if (total > max) max = total
-    }
-    for (let i in group) {
-      if (group[i].total == max) len++
-    }
-    for (let i in group) {
-      if (group[i].total == max) group[i].winnings = 2 / len
-    }
-  })
-
-  console.log(OBJ)
-
-  let ARR = []
-
-  OBJ.forEach(function (group, g) {
-    let arr = []
-    for (let i in group) {
-      arr.push(group[i])
-    }
-    ARR.push(arr)
-  })
-
-  ARR.forEach(function (group) {
-    group.sort(function (a, b) {
-      return b.total - a.total
-    })
-  })
-
-  return ARR
-}
-
-async function makePickOrder(raceid) {
-  const scores = await getScores(raceid)
-  const season = await getSeason()
-  const players = await playersObj()
-
-  let map = new Map()
-  scores.forEach(function (score) {
-    map.set(score.PLAYERID, {
-      id: score.PLAYERID,
-      name: players[score.PLAYERID].NAME,
-      rank: 0,
-    })
-  })
-  scores.forEach(function (score) {
-    map.get(score.PLAYERID).rank += parseInt(score.SCORE)
-  })
-
-  season.forEach(function (player) {
-    map.get(player.ID).rank += parseInt(player.SUM) / 1000
-  })
-  map = [...map.values()]
-  map.sort(function (a, b) {
-    return a.rank - b.rank
-  })
-  map.forEach(function(item, i) {
-    item.groupid = i % 2
-  })
-  return map
-}
-
-
-
-/*
-https://cf.nascar.com/cacher/drivers.json
-https://cf.nascar.com/cacher/live/live-feed.json
-https://cf.nascar.com/cacher/live/series_1/RACEID/live-pit-data.json
-https://cf.nascar.com/cacher/tracks.json
-https://cf.nascar.com/cacher/YYYY/1/points-feed.json
-https://cf.nascar.com/cacher/YYYY/1/RACEID/driver-recap.json
-https://cf.nascar.com/cacher/YYYY/1/RACEID/lap-averages.json
-https://cf.nascar.com/cacher/YYYY/1/RACEID/lap-notes.json
-https://cf.nascar.com/cacher/YYYY/1/RACEID/lap-times.json
-https://cf.nascar.com/cacher/YYYY/1/RACEID/live-stage-points.json
-https://cf.nascar.com/cacher/YYYY/1/RACEID/snappytv.json
-https://cf.nascar.com/cacher/YYYY/1/RACEID/weekend-feed.json
-https://cf.nascar.com/cacher/YYYY/1/schedule-feed.json
-https://cf.nascar.com/cacher/YYYY/race_list_basic.json
-https://cf.nascar.com/data/cacher/production/RACEID/1/RACEID/raceResults.json
-https://cf.nascar.com/live-ops/live-ops.json
-https://cf.nascar.com/live/feeds/live-flag-data.json
-https://cf.nascar.com/live/feeds/series_1/RACEID/live_feed.json
-https://cf.nascar.com/live/feeds/series_1/RACEID/live_points.json
-https://cf.nascar.com/loopstats/prod/YYYY/1/RACEID.json
-https://www.nascar.com/wp-json/
-https://www.nascar.com/wp-json/wp/v2/categories/14278
-https://www.nascar.com/wp-json/wp/v2/race_schedule/389629
-https://www.nascar.com/wp-json/wp/v2/weekend_schedule
-https://cf.nascar.com/data/images/carbadges/1/01.png
-https://cf.nascar.com/cacher/2025/1/final/1-owners-points.json
-https://cf.nascar.com/cacher/staging/live/live-feed.json
-//https://cf.nascar.com/cacher/2025/1/final/1-owners-points.json
-//https://cf.nascar.com/data/cacher/production/2025/1/racinginsights-points-feed.json
-//https://www.nascar.com/json/tracks/
-//https://cf.nascar.com/cacher/2026/1/schedule-combined-feed.json
-//https://www.nascar.com/json/drivers/
-//https://fantasygames.nascar.com/api/v1/live/odds/race/5585.json
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-async function playersobj() {
-  const players = await getPlayers()
-  let obj = {}
-  players.forEach(function(player) {
-    obj[player.ID] = player
-  })
-  return obj
-}
-
-async function driversobj() {
-  const drivers = await getStandings()
-  let obj = {}
-  drivers.forEach(function(driver) {
-    obj[driver.driver_id] = {
-      name: driver.driver_name,
-      id: driver.driver_id,
-      badge: driver.car_no
-    }
-  })
-  return obj
-}
-
-async function playerpicksobj(raceid) {
-  const players = await getPlayers()
-  const array = await driverpicksobj(raceid)
-  let obj = {}
-  players.forEach(function(player) {
-    obj[player.ID] = []
-  })
-  array.forEach(function(group) {
-    for (let i in group) {
-      if (group[i]) obj[group[i]].push(i)
-    }
-  })
-  return obj
-}
-
-async function driverpicksobj(raceid) {
-  const array = await getPicks(raceid)
-  let arr = []
-  array.forEach(function(item) {
-    arr[item.GROUPID] = {}
-  })
-  array.forEach(function(item) {
-    arr[item.GROUPID][item.DRIVERID] = item.PLAYERID || ""
-  })
-  return arr
-}
-
-async function pickorderobj(raceid) {
-  const array = await getPickOrder(raceid)
-  let arr = []
-  array.forEach(function(group) {
-    arr[group.GROUPID] = []
-  })
-  array.forEach(function(group) {
-    arr[group.GROUPID].push({
-      id: group.PLAYERID,
-      rank: group.RANK
-    })
-  })
-  return arr
-}
-
-async function driverscoresobj(raceid) {
-  const array = await getScores(raceid)
-  let arr = []
-  array.forEach(function(driver) {
-    arr[driver.GROUPID] = {}
-  })
-  array.forEach(function(driver) {
-    arr[driver.GROUPID][driver.DRIVERID] = driver
-  })
-  return arr
-}
-
-async function playerscoresobj(raceid) {
-  const array = await getScores(raceid)
-  let arr = []
-  array.forEach(function(driver) {
-    arr[driver.PLAYERID] = []
-  })
-  array.forEach(function(driver) {
-    arr[driver.PLAYERID].push(driver)
-  })
-  array.forEach(function(item) {
-    arr[item.PLAYERID].total = 0
-    arr[item.PLAYERID].forEach(function(d) {
-      arr[item.PLAYERID].total += parseInt(d.SCORE)
-    })
-  })
-  return arr
-}
-
-
-
